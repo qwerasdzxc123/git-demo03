0c1cebe452394dcd245033026573d0ac9163ba84
WangJin <WangJin@qq.com>
2021-07-08 12:46:30 +0800

    first master commit idea-gitdemo

---
+++ git-demo03/src/main/java/com/wj/cn/gitTest.java
@@ -0,0 +1,9 @@
+package com.wj.cn;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git01");
+        System.out.println("hello git02");
+        System.out.println("hello git03");
+    }
+}
